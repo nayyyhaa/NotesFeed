bdcd44f1b9243ef13c764dc8858bea6a7a075646
nayyyhaa <nehaguptang.ng@gmail.com>
2022-03-28 18:26:39 +0530

    refactor: change homepage description content

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,11 +4,14 @@ export const Homepage = () => {
   return (
     <>
       <header className="site-header row-flex p-t-3 p-h-1">
-        <div className="introduction m-h-1">
+        <div className="introduction m-h-2 m-t-3">
           <h1 className="title lg-text">
             Welcome to <span className="colored-text lg-text bg h1 text-stroke">NotesFeed!</span>
           </h1>
-          <p className="description h3 m-t-3">Find how much you are in touch with your mind & soul.</p>
+          <p className="h3 m-v-3">
+            Your <span className="colored-text h3">modern</span> note-taking app.
+          </p>
+          <p>Manage your daily tasks & workflow in modern way to boost your efficiency without any effort.</p>
           <Link to="/notesfeed">
             <button className="btn primary-btn m-v-2">Start Note-ing!</button>
           </Link>
