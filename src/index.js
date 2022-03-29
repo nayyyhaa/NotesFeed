6460f5e39cf5f1da2557d668d1ded84dad79d939
nayyyhaa <nehaguptang.ng@gmail.com>
2022-03-29 18:15:04 +0530

    add: mock-data + styles + dependencies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { NoteProvider } from "contexts/NoteContext";
+import { ModalProvider } from "contexts/ModelContext";
 
 // Call make Server
 makeServer();
@@ -11,7 +13,11 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <NoteProvider>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </NoteProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
