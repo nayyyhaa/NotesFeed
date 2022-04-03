2118be66e88afa2a32895b944e9b5fa089c51202
nayyyhaa <nehaguptang.ng@gmail.com>
2022-04-03 18:09:13 +0530

    fix: clear filter on route chnage

--- src/toolkit/utils/index.js
+++ src/toolkit/utils/index.js
@@ -2,3 +2,4 @@ export { formatDate } from "./formatDate";
 export { searchNote } from "./searchNote";
 export { getLabels } from "./getLabels";
 export { getLabelSelected, sort } from "./getFilters";
+export { usePageViewTracker } from "./usePageViewTracker";
