dd730a0222827ceb3750b3d13f30d2ead063ff30
nayyyhaa <nehaguptang.ng@gmail.com>
2022-04-04 15:58:34 +0530

    del: default note-data

--- src/contexts/NoteContext.js
+++ src/contexts/NoteContext.js
@@ -1,14 +1,14 @@
 import { createContext, useReducer, useContext, useState } from "react";
 import { noteReducer } from "reducers/noteReducer";
-import notesData from "toolkit/data/notesData";
 import { getLabels } from "toolkit/utils";
 
 const NoteContext = createContext();
+const initialLabels = ["School", "Work", "Personal"];
 
 const NoteProvider = ({ children }) => {
-  const [notes, dispatchNote] = useReducer(noteReducer, { allNotes: notesData ?? [], deletedNotes: [], archives: [] });
+  const [notes, dispatchNote] = useReducer(noteReducer, { allNotes: [], deletedNotes: [], archives: [] });
 
-  const [labelList, setLabelList] = useState(() => getLabels(notes));
+  const [labelList, setLabelList] = useState(() => [...initialLabels, ...getLabels(notes)]);
   return (
     <NoteContext.Provider value={{ notes, dispatchNote, labelList, setLabelList }}>{children}</NoteContext.Provider>
   );
