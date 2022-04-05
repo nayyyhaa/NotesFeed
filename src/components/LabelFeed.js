81f3b9a9f88b59208b51497210273de040d0b551
nayyyhaa <nehaguptang.ng@gmail.com>
2022-04-05 16:48:26 +0530

    fix: no note in label feed

--- src/components/LabelFeed.js
+++ src/components/LabelFeed.js
@@ -6,6 +6,7 @@ import { Note } from "./Note";
 export const LabelFeed = () => {
   const { notes, labelList } = useNote();
   const { setModalOpen } = useModal();
+  let noLabel = true;
 
   return (
     <section className="notefeed-section m-v-3">
@@ -21,22 +22,23 @@ export const LabelFeed = () => {
         return (
           <div key={label}>
             <h2 className="w-60p m-auto h3 p-v-2">{notes?.allNotes.some((note) => note.label === label) && label}</h2>
-            {notes.allNotes.length !== 0 ? (
-              notes?.allNotes?.map((note) => note.label === label && <Note key={note.id} note={note} />)
-            ) : (
-              <div className="grid-ctr m-v-5">
-                <img className="w-30p no-note" src={noNote} alt="no note" />
-                <p className="m-t-3">No note found</p>
-                <p>
-                  <strong>
-                    Start{" "}
-                    <span className="colored-text cursor" onClick={() => setModalOpen(false)}>
-                      Note-ing!
-                    </span>
-                  </strong>
-                </p>
-              </div>
-            )}
+            {notes.allNotes.length !== 0
+              ? notes?.allNotes?.map((note) => note.label === label && <Note key={note.id} note={note} />)
+              : noLabel && (
+                  <div className="grid-ctr m-v-5">
+                    <img className="w-30p no-note" src={noNote} alt="no note" />
+                    <p className="m-t-3">No note found</p>
+                    <p>
+                      <strong>
+                        Start{" "}
+                        <span className="colored-text cursor" onClick={() => setModalOpen(false)}>
+                          Note-ing!
+                          {(noLabel = false)}
+                        </span>
+                      </strong>
+                    </p>
+                  </div>
+                )}
           </div>
         );
       })}
